697cc9edb4a93c35433555c26af9525cf7fb6acd
nrshakib <nrshakib007@gmail.com>
2026-08-16 15:15:06 +0600

    Added Specialities & Available today page structure

---
+++ src/app/(public)/find-care/available-today/page.tsx
@@ -0,0 +1,10 @@
+import ComingSoon from "@/components/Shared/ComingSoon";
+import React from "react";
+
+export default function AvailableToday() {
+  return (
+    <div>
+      <ComingSoon />
+    </div>
+  );
+}
